1153213bd8d3d34b3d015e02167d198c5dee7d34
Hilbert Ruban <hilbert.r@targetbay.com>
2023-08-29 00:04:37 +0530

    implementing new project to refreshing redux

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,32 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationID: action.payload.nationID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName  };
+}
